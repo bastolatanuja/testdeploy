72ac73ba92d6fdbfcf71145db766d245c7f241c8
Niteshy85013 <79129812+Niteshy85013@users.noreply.github.com>
2022-05-30 21:17:58 +0545

    update

--- Home/urls.py
+++ Home/urls.py
@@ -4,14 +4,14 @@ from Home import views
 app_name = "Home"
 
 urlpatterns = [
-   path('home/',views.home, name='home'),
+   path('',views.home, name='home'),
    path("register/",views.register, name='register'),
    path('login/', views.login_fn, name='login'),
    path('home_fn/',views.home_fn, name='home_fn'),
    
    
    
-   path('', views.PortfolioView.as_view(), name="portfolio"),
+   path('portfolio/', views.PortfolioView.as_view(), name="portfolio"),
    path('portfolio/<slug:slug>', views.PortfolioDetailView.as_view(), name="portfolio"),
 	  
     
