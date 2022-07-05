15a4aff28e86b865c9f3d579811fb267dc14a4b5
anup7777 <burlakotianup@gmail.com>
2022-07-05 13:51:12 +0545

    sprint7_test

--- Home/tests.py
+++ Home/tests.py
@@ -1,6 +1,21 @@
-from django.test import TestCase
+from django import views
+from django.urls import reverse,resolve
+from django.test import   SimpleTestCase 
+from Home.views import * 
 
+ 
 # Create your tests here.
+
+
+class TestUrls(SimpleTestCase):
+
+    def test_case_search_url(self):
+        url=reverse('Home:search')
+        self.assertEquals(resolve(url).func,SearchView)
+
+    def test_case_searchresult_url(self):
+        url=reverse('Home:searchresult')
+        self.assertEquals(resolve(url).func,searchresult)
 
 
 
