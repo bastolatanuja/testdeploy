e51e60178d46053256ae2054efd58b09084fda26
Niteshy85013 <79129812+Niteshy85013@users.noreply.github.com>
2022-07-05 13:19:01 +0545

    sprint5_test

--- Home/tests.py
+++ Home/tests.py
@@ -1,3 +1,26 @@
-from django.test import TestCase
+from django import views
+from django.urls import reverse,resolve
+from django.test import   SimpleTestCase 
+from Home.views import * 
 
+ 
 # Create your tests here.
+
+
+def test_case_services_url(self):
+    url=reverse('Home:services')
+    self.assertEquals(resolve(url).func,services)
+
+def test_case_ambulance_url(self):
+    url=reverse('Home:ambulance')
+    self.assertEquals(resolve(url).func,ambulance) 
+
+def test_case_doctors_url(self):
+    url=reverse('Home:doctors')
+    self.assertEquals(resolve(url).func,doctors)  
+
+def test_case_helpsection_url(self):
+    url=reverse('Home:helpsection')
+    self.assertEquals(resolve(url).func,helpsection)   
+
+  
