317cf2e2bf93f9c883abd8f6bc332a78ec4e36a1
kishan-pj <78782776+kishan-pj@users.noreply.github.com>
2022-07-05 13:33:09 +0545

    sprint3_test

--- Home/tests.py
+++ Home/tests.py
@@ -5,11 +5,12 @@ from Home.views import *
 
 # Create your tests here.
 
-def test_case_logout_url(self):
-    url=reverse('Home:logout')
-    self.assertEquals(resolve(url).func,logout)      
-    
-def test_case_shop_url(self):
-    url=reverse('Home:shop')
-    self.assertEquals(resolve(url).func,shop) 
+class TestUrls(SimpleTestCase):
 
+    def test_case_logout_url(self):
+        url=reverse('Home:logout')
+        self.assertEquals(resolve(url).func,logout)      
+    
+    def test_case_portfolio_url(self):
+        url=reverse('Home:portfolio')
+        self.assertEquals(resolve(url).func,PortfolioView) 
