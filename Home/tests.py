afae4ce3ff2226d0cadbf754cc57035d58516483
Niteshy85013 <79129812+Niteshy85013@users.noreply.github.com>
2022-07-05 13:36:02 +0545

    sprint5_test

--- Home/tests.py
+++ Home/tests.py
@@ -7,20 +7,22 @@ from Home.views import *
 # Create your tests here.
 
 
-def test_case_services_url(self):
-    url=reverse('Home:services')
-    self.assertEquals(resolve(url).func,services)
+class TestUrls(SimpleTestCase):
 
-def test_case_ambulance_url(self):
-    url=reverse('Home:ambulance')
-    self.assertEquals(resolve(url).func,ambulance) 
+    def test_case_services_url(self):
+        url=reverse('Home:services')
+        self.assertEquals(resolve(url).func,services)
 
-def test_case_doctors_url(self):
-    url=reverse('Home:doctors')
-    self.assertEquals(resolve(url).func,doctors)  
+    def test_case_ambulance_url(self):
+        url=reverse('Home:ambulance')
+        self.assertEquals(resolve(url).func,ambulance) 
 
-def test_case_helpsection_url(self):
-    url=reverse('Home:helpsection')
-    self.assertEquals(resolve(url).func,helpsection)   
+    def test_case_doctors_url(self):
+        url=reverse('Home:doctors')
+        self.assertEquals(resolve(url).func,doctors)  
+
+    def test_case_helpsection_url(self):
+        url=reverse('Home:helpsection')
+        self.assertEquals(resolve(url).func,helpsection)   
 
   
