908b60ec143626d9acad4b021ae420064743ec6e
kishan-pj <78782776+kishan-pj@users.noreply.github.com>
2022-07-05 13:28:41 +0545

    sprint4_test

--- Home/tests.py
+++ Home/tests.py
@@ -6,16 +6,18 @@ from Home.views import *
  
 # Create your tests here.
 
-def test_case_forgotpassword_url(self):
+class TestUrls(SimpleTestCase):
+
+    def test_case_forgotpassword_url(self):
         url=reverse('Home:password_reset')
         self.assertEquals(resolve(url).func,password_reset_request) 
 
-def test_case_shop_url(self):
+    def test_case_shop_url(self):
         url=reverse('Home:shop')
         self.assertEquals(resolve(url).func,shop) 
 
 
-def test_case_contactus_url(self):
+    def test_case_contactus_url(self):
         url=reverse('Home:contactus')
         self.assertEquals(resolve(url).func,contact_us) 
 
