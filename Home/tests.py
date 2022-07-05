e7f7beae40433fff1193cd4d69e2295f3a52ad59
Ishworkhadka99123 <79239793+Ishworkhadka99123@users.noreply.github.com>
2022-07-05 13:25:46 +0545

    sprint6_test

--- Home/tests.py
+++ Home/tests.py
@@ -1,8 +1,28 @@
-from django.test import TestCase
+from django import views
+from django.urls import reverse,resolve
+from django.test import   SimpleTestCase 
+from Home.views import * 
 
+ 
 # Create your tests here.
 
 
+class TestUrls(SimpleTestCase):
+
+    def test_case_prescription_url(self):
+        url=reverse('Home:prescription')
+        self.assertEquals(resolve(url).func,prescription) 
+    
+    def test_case_cart_url(self):
+        url=reverse('Home:cart')
+        self.assertEquals(resolve(url).func,cart)  
+        
+    def test_case_addcart_url(self):
+        url=reverse('Home:addcart',args=[1])
+        self.assertEquals(resolve(url).func,addcart)
+     
+
+    
 
 
 
